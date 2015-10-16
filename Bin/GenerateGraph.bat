3cbef26ba2d3c705d3e2ca1456aecaf4d2054540
Sebastian Klatte <sebastianklatte@gmx.net>
2015-10-16 19:10:14 +0200

    Added missing quotes

--- Bin/GenerateGraph.bat
+++ Bin/GenerateGraph.bat
@@ -7,14 +7,14 @@ rem 1. Process the log of stack traces and produce a graph in the text based dot
 rem 2. Generate a nicely layouted graphic diagram out of the dot file (dot.exe)
 
 rem Step 1 - generate dot file
-%~dp0RefCountTracer.exe "%1" "%1.dot" %3 %4 %5 %6 %7
+"%~dp0RefCountTracer.exe" "%1" "%1.dot" %3 %4 %5 %6 %7
 
 if not errorlevel 0 goto exit
 
 rem Step 2 - generate a diagram
 
 rem Path to dot.exe (part of Graphviz -> http://www.graphviz.org/)
-set dot=%~dp0Tools\graphviz\win32\bin\dot.exe
+set dot="%~dp0Tools\graphviz\win32\bin\dot.exe"
 
 rem Output format. Other valid formats are png, pdf, svg, and all listed here: http://www.graphviz.org/content/output-formats
 set format=svg
